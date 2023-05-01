0e8fac19c280c9e93e54e73686212bfab589ae47
AndresAtala <132239849+AndresAtala@users.noreply.github.com>
2023-04-30 22:06:56 -0400

    Add files via upload

---
+++ powerLifter.java
@@ -0,0 +1,14 @@
+public class powerLifter {
+    private String Nombre;
+    private int edad;
+    private double peso, pressBanca, sentadilla, pesoMuerto;
+
+    public powerLifter(String Nombre, int edad, double peso, double pressBanca, double sentadilla, double pesoMuerto) {
+        this.Nombre = Nombre;
+        this.edad = edad;
+        this.peso = peso;
+        this.pressBanca = pressBanca;
+        this.sentadilla = sentadilla;
+        this.pesoMuerto = pesoMuerto;
+    }
+}
