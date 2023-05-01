a8a0afe88f6edef8c707380368fbaa67aaf572ac
AndresAtala <132239849+AndresAtala@users.noreply.github.com>
2023-04-30 22:09:19 -0400

    Add files via upload
    final version

--- powerLifter.java
+++ powerLifter.java
@@ -11,4 +11,51 @@ public class powerLifter {
         this.sentadilla = sentadilla;
         this.pesoMuerto = pesoMuerto;
     }
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.Nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setedad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getpeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getPressBanca() {
+        return pressBanca;
+    }
+    public void setPressBanca(double press_banca) {
+        this.pressBanca = press_banca;
+    }
+    public double getSentadilla(){
+        return sentadilla;
+    }
+    public void setSentadilla(double sentadilla) {
+        this.sentadilla = sentadilla;
+    }
+    public double getPesoMuerto(double peso_muerto) {
+        return peso_muerto;
+    }
+    public void setPesoMuerto(double peso_muerto){
+        this.pesoMuerto = peso_muerto;
+    }
+
+    @Override
+    public String toString(){
+        return Nombre + "" + edad + "" + peso + "" + pressBanca + "" + sentadilla + "" + pesoMuerto;
+    }
 }
